35be5461aadf591f3e49d29d64f1acc246b5eadb
Fynn Schotten <fynnshotten@gmail.com>
2025-05-09 10:49:35 +0200

    fix duplicatipon

---
+++ IngrediScan/View/Component/MinimalRecipeCard.swift
@@ -0,0 +1,18 @@
+//
+//  MinimalRecipeCard.swift
+//  IngrediScan
+//
+//  Created by Faramir on 09.05.25.
+//
+
+import SwiftUI
+
+struct MinimalRecipeCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MinimalRecipeCard()
+}
