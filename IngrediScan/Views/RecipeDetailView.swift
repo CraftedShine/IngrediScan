9956164ca883aead6f43314a4c6fc4acf6e52db9
Fynn Schotten <fynnshotten@gmail.com>
2025-05-26 19:41:44 +0200

    wip

--- IngrediScan/Views/RecipeDetailView.swift
+++ IngrediScan/Views/RecipeDetailView.swift
@@ -6,40 +6,42 @@ struct RecipeDetailView: View {
     @State private var detailedCooking: Bool = false
     
     var body: some View {
-        ScrollView {
-            ZStack(alignment: .topLeading) {
-                VStack {
-                    VStack(alignment: .leading) {
-                        RecipeImage(recipe: $recipe)
-                        TagList(tags: recipe.tags)
-                        CookingStatistics(recipe: recipe)
-                            .padding()
-                        IngredientList(recipe: recipe)
-                            .padding()
+        NavigationView {
+            ScrollView {
+                ZStack(alignment: .topLeading) {
+                    VStack {
+                        VStack(alignment: .leading) {
+                            RecipeImage(recipe: $recipe)
+                            TagList(tags: recipe.tags)
+                            CookingStatistics(recipe: recipe)
+                                .padding()
+                            IngredientList(recipe: recipe)
+                                .padding()
+                        }
+                        
+                        Button {
+                            detailedCooking.toggle()
+                        } label: {
+                            Text("Kochen starten")
+                                .font(.callout .bold() .smallCaps())
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .padding(.bottom)
                     }
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(20)
+                    .padding()
+                    .shadow(radius: 5)
                     
-                    Button {
-                        detailedCooking.toggle()
-                    } label: {
-                        Text("Kochen starten")
-                            .font(.callout .bold() .smallCaps())
-                    }
-                    .buttonStyle(.borderedProminent)
-                    .padding(.bottom)
+                    DismissButton()
                 }
-                .background(Color(UIColor.secondarySystemBackground))
-                .cornerRadius(20)
-                .padding()
-                .shadow(radius: 5)
-                
-                DismissButton()
             }
-        }
-        .scrollIndicators(.hidden)
-        .sheet(isPresented: $detailedCooking) {
-            CookingView(recipe: $recipe)
-                .presentationDetents([.fraction(0.99)])
-                .presentationBackground(.clear)
+            .scrollIndicators(.hidden)
+            .navigationDestination(isPresented: $detailedCooking, destination: {
+                CookingView(recipe: $recipe)
+                    .presentationDetents([.fraction(0.99)])
+                    .presentationBackground(.clear)
+            })
         }
     }
 }
