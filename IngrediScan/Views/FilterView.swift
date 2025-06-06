5a4ae50d7a200bac54728a1e968a5b302c12e986
Fynn Schotten <fynnshotten@gmail.com>
2025-06-06 09:05:43 +0200

    Scroll view for filterview

--- IngrediScan/Views/FilterView.swift
+++ IngrediScan/Views/FilterView.swift
@@ -5,7 +5,7 @@ struct StarRatingView: View {
     var maximumRating: Int = 5
     var onColor = Color.yellow
     var offColor = Color.gray
-
+    
     var body: some View {
         HStack(spacing: 8) {
             ForEach(1...maximumRating, id: \.self) { number in
@@ -33,94 +33,96 @@ struct FilterView: View {
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 16) {
-                // MARK: Sort by
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("Sort by")
-                        .font(.headline .bold() .smallCaps())
-                        .padding(.top)
-                    
-                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 12), count: 2), spacing: 12) {
-                        ForEach(sortOptions, id: \.self) { option in
-                            Button(action: {
-                                selectedSort = option
-                            }) {
-                                Text(option)
-                                    .frame(maxWidth: .infinity)
-                                    .padding()
-                                    .background(selectedSort == option ? Color.blue.opacity(0.1) : Color(.systemGray6))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(selectedSort == option ? Color.blue : Color.clear, lineWidth: 2)
-                                    )
-                                    .cornerRadius(10)
+            ScrollView {
+                VStack(spacing: 16) {
+                    // MARK: Sort by
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Sort by")
+                            .font(.headline .bold() .smallCaps())
+                            .padding(.top)
+                        
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 12), count: 2), spacing: 12) {
+                            ForEach(sortOptions, id: \.self) { option in
+                                Button(action: {
+                                    selectedSort = option
+                                }) {
+                                    Text(option)
+                                        .frame(maxWidth: .infinity)
+                                        .padding()
+                                        .background(selectedSort == option ? Color.blue.opacity(0.1) : Color(.systemGray6))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(selectedSort == option ? Color.blue : Color.clear, lineWidth: 2)
+                                        )
+                                        .cornerRadius(10)
+                                }
                             }
                         }
                     }
-                }
-                
-                Divider()
-                
-                HStack {
-                    Text("Rating")
-                    Spacer()
-                    StarRatingView(rating: $rating)
-                }
-                
-                HStack {
-                    Text("Calories")
                     
-                    Spacer()
+                    Divider()
                     
-                    TextField("", value: $minCalories, formatter: NumberFormatter())
-                        .frame(width: 75)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.numberPad)
-                        .multilineTextAlignment(.trailing)
-                    
-                    Text("to")
-                    
-                    TextField("", value: $maxCalories, formatter: NumberFormatter())
-                        .frame(width: 75)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.numberPad)
-                        .multilineTextAlignment(.trailing)
-                    
-                }
-                
-                Toggle("Show Favorites", isOn: $showFavorites)
-                    .toggleStyle(SwitchToggleStyle(tint: .blue))
-                
-                Spacer()
-                
-                // MARK: Show Recipes Button
-                Button(action: {
-                    // Do something
-                    dismiss()
-                }) {
-                    Text("Show Recipes")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(12)
-                }
-                .padding(.bottom, 16)
-            }
-            .padding()
-            .navigationTitle("Filter")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .cancellationAction) {
-                    Button("Cancel") {
-                        dismiss()
+                    HStack {
+                        Text("Rating")
+                        Spacer()
+                        StarRatingView(rating: $rating)
                     }
+                    
+                    HStack {
+                        Text("Calories")
+                        
+                        Spacer()
+                        
+                        TextField("", value: $minCalories, formatter: NumberFormatter())
+                            .frame(width: 75)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+                            .multilineTextAlignment(.trailing)
+                        
+                        Text("to")
+                        
+                        TextField("", value: $maxCalories, formatter: NumberFormatter())
+                            .frame(width: 75)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.numberPad)
+                            .multilineTextAlignment(.trailing)
+                        
+                    }
+                    
+                    Toggle("Show Favorites", isOn: $showFavorites)
+                        .toggleStyle(SwitchToggleStyle(tint: .blue))
+                    
+                    Spacer()
+                    
+                    // MARK: Show Recipes Button
+                    Button(action: {
+                        // Do something
+                        dismiss()
+                    }) {
+                        Text("Show Recipes")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(12)
+                    }
+                    .padding(.bottom, 16)
                 }
-                ToolbarItem(placement: .confirmationAction) {
-                    Button("Reset") {
-                        // Reset logic
-                        selectedSort = "Calories"
-                        showFavorites = false
+                .padding()
+                .navigationTitle("Filter")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Reset") {
+                            // Reset logic
+                            selectedSort = "Calories"
+                            showFavorites = false
+                        }
                     }
                 }
             }
