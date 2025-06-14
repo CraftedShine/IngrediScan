8ece5e8ce1538df20ff12e584ae8c75d5d1b1d13
aixJan <janh.keller@web.de>
2025-06-14 11:22:47 +0200

    *worked on sorting

---
+++ IngrediScan/Views/SearchScreenViews/SearchForm.swift
@@ -0,0 +1,18 @@
+//
+//  SearchForm.swift
+//  IngrediScan
+//
+//  Created by Jan Keller on 14.06.25.
+//
+
+import SwiftUI
+
+struct SearchForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchForm()
+}
